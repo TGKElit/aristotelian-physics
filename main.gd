d1d69cb6c36b8f7a94898665eeba1e60f331a77a
Niklas Johansson <tgkelit@gmail.com>
2026-05-29 17:59:08 +0200

    Consolidated, typed, and organized variables

--- main.gd
+++ main.gd
@@ -1,56 +1,71 @@
 extends Node2D
-var viewport_width
-var viewport_height
+var viewport_width: float
+var viewport_height: float
+var aspect_ratio: Array
 
-var grid_width
-var cell_width
-var cell_height
-var grid_height
-var grid_scalar = 64
+var grid_width: int
+var grid_height: int
+var cell_width: float
+var cell_height: float
+var grid_scalar: int = 1
 
-var grid = []
+var grid: Array = []
+
+var element_purity: int = 200
+
+var earth: Dictionary = {
+	density = 256,
+	color = Color(0.4, 0.25, 0.2)
+	}
+var water: Dictionary = {
+	density = 3,
+	color = Color(0, 0, 1)
+	}
+var air: Dictionary = {
+	density = 2,
+	color = Color(0.6, 0.8, 1)
+	}
+var fire: Dictionary = {
+	density = 1,
+	color = Color(1, 1, 0)
+	}
 
 var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	viewport_width = get_viewport_rect().size.x
 	viewport_height = get_viewport_rect().size.y
-
-	grid_width = 16 * grid_scalar
-	grid_height = 9 * grid_scalar
+	aspect_ratio = [16, 9]
+	
+	grid_width = aspect_ratio[0] * grid_scalar
+	grid_height = aspect_ratio[1] * grid_scalar
 	cell_width = viewport_width / grid_width
 	cell_height = viewport_height / grid_height
 
 	for x in grid_width:
 		grid.append([])
 		for y in grid_height:
-			var element_purity = 200
-			var earth = pow(rng.randf(), element_purity)
-			var water = pow(rng.randf(), element_purity)
-			var air = pow(rng.randf(), element_purity)
-			var fire = pow(rng.randf(), element_purity)
+			var earth_con: float = pow(rng.randf(), element_purity)
+			var water_con: float = pow(rng.randf(), element_purity)
+			var air_con: float = pow(rng.randf(), element_purity)
+			var fire_con: float = pow(rng.randf(), element_purity)
+			var element_sum: float = earth_con + water_con + air_con + fire_con 
 			
-			var element_sum = earth+water+air+fire 
+			
 			
 			grid[x].append({
-				earth = earth / element_sum,
-				water = water / element_sum,
-				air = air / element_sum,
-				fire = fire / element_sum
+				earth = earth_con / element_sum,
+				water = water_con / element_sum,
+				air = air_con / element_sum,
+				fire = fire_con / element_sum
 			})
 	
 func _draw():
 	for x in grid_width:
 		for y in grid_height:
 			
-			var color = Color(
-				#red
-				(0.4*grid[x][y].earth + 0*grid[x][y].water + 0.6*grid[x][y].air + grid[x][y].fire), # red
-				#green
-				(0.25*grid[x][y].earth +  0*grid[x][y].water + 0.8*grid[x][y].air + grid[x][y].fire*(1-rng.randf_range(0.0,0.1))), # green
-				#blue
-				(0.2*grid[x][y].earth + grid[x][y].water + grid[x][y].air + 0*grid[x][y].fire) # blue
-			)
+			var color: Color = Color(earth.color, grid[x][y].earth).blend(Color(water.color, grid[x][y].water)).blend(Color(air.color, grid[x][y].air)).blend(Color(fire.color, grid[x][y].fire))
+			
 			draw_rect(Rect2(x * cell_width, y * cell_height, cell_width, cell_height), color)
 	
 
@@ -60,75 +75,26 @@ func _process(_delta: float) -> void:
 		#var row_weight_sum = 0
 		for x in grid_width:
 			
-			var earth = grid[x][y].earth
-			var water = grid[x][y].water
-			var air = grid[x][y].air
-			var fire = grid[x][y].fire
-			var earth_down = grid[x][y+1].earth
-			var water_down = grid[x][y+1].water
-			var air_down = grid[x][y+1].air
-			var fire_down = grid[x][y+1].fire
+			var composition: Dictionary = grid[x][y]
+			var composition_down: Dictionary = grid[x][y+1]
+			var composition_reverse: Dictionary = grid[x][grid_height-y-1]
+			var composition_reverse_up: Dictionary = grid[x][grid_height-y-2]
+			var composition_left: Dictionary = grid[x-1][y]
 			
-			var earth_reverse = grid[x][(grid_height - 1) - y].earth
-			var water_reverse = grid[x][(grid_height - 1) - y].water
-			var air_reverse = grid[x][(grid_height - 1) - y].air
-			var fire_reverse = grid[x][(grid_height - 1) - y].fire
-			var earth_reverse_up = grid[x][(grid_height - 1) - y - 1].earth
-			var water_reverse_up = grid[x][(grid_height - 1) - y - 1].water
-			var air_reverse_up = grid[x][(grid_height - 1) - y - 1].air
-			var fire_reverse_up = grid[x][(grid_height - 1) - y - 1].fire
 			
-			var earth_left = grid[x-1][y].earth
-			var water_left = grid[x-1][y].water
-			var air_left = grid[x-1][y].air
-			var fire_left = grid[x-1][y].fire
+			if weight(composition) > weight(composition_down):
+				grid[x][y] = composition_down
+				grid[x][y+1] = composition
 			
-			if weight(earth, water, air, fire) > weight(earth_down, water_down, air_down, fire_down):
-				grid[x][y] = {
-					earth = earth_down,
-					water = water_down,
-					air = air_down,
-					fire = fire_down
-				}
-				grid[x][y+1] = {
-					earth = earth,
-					water = water,
-					air = air,
-					fire = fire
-				}
-			
-			if weight(earth_reverse, water_reverse, air_reverse, fire_reverse) < weight(earth_reverse_up, water_reverse_up, air_reverse_up, fire_reverse_up):
-				grid[x][(grid_height-1)-y] = {
-					earth = earth_reverse_up,
-					water = water_reverse_up,
-					air = air_reverse_up,
-					fire = fire_reverse_up
-				}
-				grid[x][(grid_height-1)-y-1] = {
-					earth = earth_reverse,
-					water = water_reverse,
-					air = air_reverse,
-					fire = fire_reverse
-				}
+			if weight(composition_reverse) < weight(composition_reverse_up):
+				grid[x][grid_height-y-1] = composition_reverse_up
+				grid[x][grid_height-y-2] = composition_reverse
 				
-			earth = grid[x][y].earth
-			water = grid[x][y].water
-			air = grid[x][y].air
-			fire = grid[x][y].fire
+			composition = grid[x][y]
 				
-			if rng.randf()*weight(earth, water, air, fire)*weight(earth_left, water_left, air_left, fire_left) / 16 < 0.05:
-				grid[x][y] = {
-					earth = earth_left,
-					water = water_left,
-					air = air_left,
-					fire = fire_left
-				}
-				grid[x-1][y] = {
-					earth = earth,
-					water = water,
-					air = air,
-					fire = fire
-				}
+			if rng.randf()*weight(composition)*weight(composition_left) / 16 < 0.05:
+				grid[x][y] = composition_left
+				grid[x-1][y] = composition
 					
 			#row_weight_sum += min(weight(earth, water, air, fire), weight(earth_down, water_down, air_down, fire_down))
 			queue_redraw()
@@ -136,5 +102,5 @@ func _process(_delta: float) -> void:
 	# $HUD.update_weight(row_weight_sums_string)
 	
 
-func weight(earth, water, air, fire) -> float:
-	return 256 * earth + 2 * water + 1.5 * air + 1 * fire
+func weight(composition: Dictionary) -> float:
+	return composition.earth * earth.density + composition.water * water.density + composition.air * air.density + composition.fire * fire.density
